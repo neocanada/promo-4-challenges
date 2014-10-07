703492c2a492979f838631cb305291b979ff542f
Sebastien <slemoan@gmail.com>
2014-10-07 16:42:45 +0200

    resultat

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,8 +1,25 @@
 def coach_answer(your_message)
   # TODO: return coach answer to your_message
+  if your_message.split("").last == "?"
+    return "Silly question, get dressed and go to work!"
+  elsif your_message == "I am going to work right now!"
+    return ""
+  else
+    return "I don't care, get dressed and go to work!"
+  end
 end
 
 def coach_answer_enhanced(your_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours!
+  if your_message == "I AM GOING TO WORK RIGHT NOW!"
+    return ""
+  elsif your_message == your_message.upcase
+    return "I can feel your motivation! #{coach_answer(your_message)}"
+  elsif your_message.split("").last == "?"
+    return "Silly question, get dressed and go to work!"
+  elsif your_message == "I am going to work right now!"
+    return ""
+  else "I don't care, get dressed an go to work"
+  end
 end
 
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,8 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+puts "Just do it baby"
+message = gets.chomp
+while coach_answer_enhanced(message) != ""
+  puts coach_answer_enhanced(message)
+  message = gets.chomp
+end
