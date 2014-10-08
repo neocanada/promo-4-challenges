6f7d81e462aad50063e240b71421fcb1a7ebd7e4
Sebastien <slemoan@gmail.com>
2014-10-08 22:27:46 +0200

    resultat

--- 01-Ruby/02-Arrays-Enumerable/05-Destructive-Methods/lib/pmu.rb
+++ 01-Ruby/02-Arrays-Enumerable/05-Destructive-Methods/lib/pmu.rb
@@ -1,5 +1,6 @@
 def pmu_format!(race_array)
   # TODO: modify the given array so that it is PMU-consistent. This method should return nil.
+  race_array.map! { |n| (race_array.index(n) + 1).to_s + "-" + n + "!" }.reverse!
 end
 
 
