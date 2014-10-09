ca239c2a9b88598180288bf36734339653612363
Sebastien <slemoan@gmail.com>
2014-10-09 15:42:26 +0200

    resultat

--- 01-Ruby/03-Blocks/03-Beautify-my-code/lib/chain_unchain.rb
+++ 01-Ruby/03-Blocks/03-Beautify-my-code/lib/chain_unchain.rb
@@ -1,12 +1,8 @@
 def shuffle_word(a_word)
   # TODO: refactor this method
 
-  cap_word = a_word.upcase
-  chars_enum = cap_word.chars
-  array = chars_enum.to_a
-  shuffle_array = array.shuffle
+  a_word.upcase.chars.to_a.shuffle
 
-  shuffle_array
 
 end
 
@@ -14,5 +10,10 @@ end
 def quote_prime_numbers(n)
   # TODO: refactor this method
 
-  (1..n).find_all {|i| (2..i-1).select {|k| i % k == 0 }.count == 0 }.map{ |prime_num| "#{prime_num} is prime"}
+  #(1..n).find_all {|i| (2..i-1).select {|k| i % k == 0 }.count == 0 }.map{ |prime_num| "#{prime_num} is prime"}
+
+  nombre_premier = (1..n).select { |i| (2..i - 1).select { |k| i % k == 0 }.count == 0 }
+  nombre_premier.map do |prime_num|
+  "#{prime_num} is prime"
+  end
 end
