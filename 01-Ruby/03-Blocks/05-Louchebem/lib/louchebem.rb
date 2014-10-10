abf860902b041ba5fe361be901bb9767c7caad83
Sebastien <slemoan@gmail.com>
2014-10-10 11:17:31 +0200

    a revoir

--- 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
@@ -2,4 +2,13 @@
 
 def louchebemize(sentence)
   # TODO: implement your louchebem translator
+
+suffix = ["em", "é", "ji", "oc", "ic", "uche", "ès"]
+vowel = ["a", "e", "i", "o", "u"]
+
+if sentence.length > 1 && sentence[0] =!
+  if sentence[0] =!
+
 end
+end
+
