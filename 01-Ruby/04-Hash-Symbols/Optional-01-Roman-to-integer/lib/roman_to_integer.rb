470ba48f42305681cb19ce4aae833b55741fd748
Sebastien <slemoan@gmail.com>
2014-10-11 10:49:53 +0200

    resultat non compété

--- 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
@@ -7,3 +7,13 @@ puts roman_to_integer('IV') == 4 # => true
 puts roman_to_integer('XII') == 12 # => true
 puts roman_to_integer('XIX') == 19 # => true
 
+
+min = 1
+max = 2
+sum = 0
+while min <= max
+sum = sum min
+min += 1
+end
+
+puts sum
