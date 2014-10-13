bb5bc0df4af80b6490569ccb88d98490fd8496e8
Sebastien <slemoan@gmail.com>
2014-10-13 12:28:07 +0200

    resultat sans offenses

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,16 +1,11 @@
 def french_phone_number?(phone_number)
-  # TODO: true or false?
+  phone_number = phone_number.gsub(" ", "").gsub("-", "")
 
-#phone_number = nil
-
-phone_number = phone_number.gsub(" ", "").gsub("-", "")
-
-if phone_number.match(/^0[^0]\d{8}$/)
-  return true
-elsif phone_number.match(/^\+33[^0]\d{8}$/)
-  return true
-else
-  return false
-end
-
+  if phone_number.match(/^0[^0]\d{8}$/)
+    return true
+  elsif phone_number.match(/^\+33[^0]\d{8}$/)
+    return true
+  else
+    return false
+  end
 end
