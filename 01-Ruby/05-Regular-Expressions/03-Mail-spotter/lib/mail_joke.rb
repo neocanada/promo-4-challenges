ca99e9d1d76232d9d8492302ad8af3eecbcd4970
Sebastien <slemoan@gmail.com>
2014-10-13 17:50:25 +0200

    resultat

--- 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
+++ 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
@@ -1,5 +1,25 @@
-# Encoding: utf-8
+PROVIDER = {
+  "hotmail.fr" => "Come on ",
+  "lewagon.org" => "Well done ",
+  "gmail.com" => "",
+  "live.com" => ""
+}
+MAIL_JOKES = {
+  "hotmail.fr" => "aren't you born before 1973?",
+  "lewagon.org" => "you're skilled and capable",
+  "gmail.com" => "you're an average but modern person",
+  "live.com" => "aren't you born before 1973?"
+}
 
 def mail_joke(email)
-  # TODO: Return (not print!) a joke suited to the email provided
+  name = email.match(/^(\w+.+)@/)
+  domaine_name = email.match(/@([a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z)/)
+
+  if email.match(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i).nil?
+    return fail ArgumentError
+  elsif email.include?(domaine_name[1])
+    return "#{PROVIDER[domaine_name[1]]}#{name[1].gsub(/\./, " ")}, #{MAIL_JOKES[domaine_name[1]]}"
+  else
+    return "Sorry #{name[1]}, we don't know how to judge '#{domaine_name[1]}'"
+  end
 end
