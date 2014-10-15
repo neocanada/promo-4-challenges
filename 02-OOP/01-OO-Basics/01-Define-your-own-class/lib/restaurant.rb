91199bcdec1b85c3f23613dcbdd17693929c267d
Sebastien <slemoan@gmail.com>
2014-10-15 11:22:35 +0200

    resultat

---
+++ 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/restaurant.rb
@@ -0,0 +1,20 @@
+
+
+
+class Restaurant
+
+attr_accessor :ville, :cuisine, :type
+
+
+  def initialize(ville, cuisine, type, tel)
+    @ville = ville
+    @cuisine = cuisine
+    @type = type
+    @tel = tel
+  end
+
+end
+
+new_resto = Restaurant.new("Cannes", "Poisson", "Gastro", "0304049484")
+
+
