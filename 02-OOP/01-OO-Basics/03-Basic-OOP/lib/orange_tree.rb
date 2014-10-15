b940958213affda96e7ace8b656b50d285c0f3ec
Sebastien <slemoan@gmail.com>
2014-10-15 13:36:59 +0200

    resultat

--- 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
+++ 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
@@ -1,3 +1,39 @@
 class OrangeTree
-  # TODO: Implement all the specs defined in the README.md :)
+  attr_accessor :height, :age, :fruits
+  def initialize
+    @age = 0
+    @height = 0
+    @fruits = 0
+  end
+
+  def one_year_passes!
+    @age += 1
+    @height += 1 if @height < 10
+    if @age > 5 && @age <= 10
+      @fruits = 100
+    elsif @age >= 10 && @age <= 15
+      @fruits = 200
+    else
+      @fruits = 0
+    end
+  end
+
+  def dead?
+    if @age <= 50
+      false
+    elsif @age >= 100
+      true
+    elsif @age - 50 < [*1..50].sample
+      true
+    end
+  end
+
+  def pick_a_fruit!
+    @fruits -= 1
+  end
 end
+
+# new_tree = OrangeTree.new
+# 100.times do new_tree.one_year_passes! end
+# p new_tree.age
+# p new_tree.dead?
