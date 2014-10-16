819e044ccfb7db296b7055f122a4da5d0caa5f1f
Sebastien <slemoan@gmail.com>
2014-10-16 17:29:40 +0200

    resultat 2

--- 02-OOP/02-OO-Advanced/Optional-01-Web-Browser/lib/browser.rb
+++ 02-OOP/02-OO-Advanced/Optional-01-Web-Browser/lib/browser.rb
@@ -2,14 +2,9 @@ require 'nokogiri'
 require 'open-uri'
 
 class Browser
-
   def fetch_content(url)
     Nokogiri::HTML(open(url)) do |config|
-    config.strict.nonet
+      config.strict.nonet
     end
   end
-
 end
-
-# recherche = Browser.new
-# p recherche.fetch_content("http://fr.wikipedia.org/wiki/Wikip%C3%A9dia")
