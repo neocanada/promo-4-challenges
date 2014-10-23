5c5d767cb8fe4b0090066b66e7c808792c09e4f3
Sebastien <slemoan@gmail.com>
2014-10-23 16:52:31 +0200

    resultat

--- 03-AR-Database/01-DB-Design-and-SQL/04-SQL-queries/lib/join_queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/04-SQL-queries/lib/join_queries.rb
@@ -2,7 +2,7 @@ require 'sqlite3'
 
 def detailed_tracks(db)
   # TODO: return the list of tracks with their album and artist
-  array = db.execute("SELECT tr.name, alb.title, ar.name
+  db.execute("SELECT tr.name, alb.title, ar.name
               FROM tracks tr
               JOIN albums alb ON (tr.album_id = alb.id)
               JOIN artists ar ON (alb.artist_id = ar.id);")
@@ -18,7 +18,7 @@ def stats_on(db, genre_name)
                       JOIN genres g ON (g.id = t.genre_id)
                       WHERE g.name = '#{genre_name}';").flatten
 
-  return {category: "#{genre_name}", number_of_songs: array[0], avg_length: array[1]}
+  return { category: "#{genre_name}", number_of_songs: array[0], avg_length: array[1] }
 end
 
 def top_five_artists(db, genre_name)
