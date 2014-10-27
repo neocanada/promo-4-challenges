24698cac9332a76635632f3e2070694d5e306aab
Sebastien <slemoan@gmail.com>
2014-10-27 16:38:19 +0100

    resultat

--- 03-AR-Database/03-ActiveRecord-Basics/03-Seed-Faker/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/03-Seed-Faker/db/seeds.rb
@@ -1,3 +1,12 @@
 require "faker"
 
 # TODO: Write a seed to insert 100 posts in the database
+
+100.times do
+  post = Post.new({
+    name: Faker::Company.name,
+    url: Faker::Internet.url,
+    votes: (0..5).to_a.sample
+  })
+  post.save
+end
