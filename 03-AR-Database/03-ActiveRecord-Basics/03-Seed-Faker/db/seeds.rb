9cca7a5b852cd8ab41a0779e46f622ab5f1227c6
Sebastien <slemoan@gmail.com>
2014-10-27 17:28:53 +0100

    resultat

--- 03-AR-Database/03-ActiveRecord-Basics/03-Seed-Faker/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/03-Seed-Faker/db/seeds.rb
@@ -3,10 +3,10 @@ require "faker"
 # TODO: Write a seed to insert 100 posts in the database
 
 100.times do
-  post = Post.new({
+  post = Post.new(
     name: Faker::Company.name,
     url: Faker::Internet.url,
     votes: (0..5).to_a.sample
-  })
+  )
   post.save
 end
--- 03-AR-Database/03-ActiveRecord-Basics/04-Seed-API/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/04-Seed-API/db/seeds.rb
@@ -2,3 +2,13 @@ require "json"
 require "rest-client"
 
 # TODO: Write a seed to insert 100 posts in the database fetched from the Hacker News API.
+
+url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+ids = JSON.parse(RestClient.get url)
+
+ids[0..9].each do |id|
+  # details = "https://hacker-news.firebaseio.com/v0/item/#{id}.json"
+  json = JSON.parse(RestClient.get "https://hacker-news.firebaseio.com/v0/item/#{id}.json")
+  post = Post.new(name: json['title'], url: json['url'], votes: json['score'])
+  post.save
+end
