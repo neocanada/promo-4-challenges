12c5d4f2e0257fb11143bbfb8623ac4569f5b5f0
Sebastien <slemoan@gmail.com>
2014-10-28 12:30:18 +0100

    resultat

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   # TODO: Add some validation
+  validates :name, :url, :user, presence: true
+  validates :name, length: { minimum: 5 }
+  validates :name.downcase, uniqueness: { case_sensitive: false }
+  validates :url, format: { with: /\Ahttps?\:\/\/(?:www\.|)?\w+\.\w{2,4}(?:\/\w+)?/, message: "invalid url" }
 end
--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
   has_many :posts
 
   # TODO: Add some validation
+  before_validation :stripspaces
+  validates :username, :email, presence: true
+  validates :email, format: { with: /\A\w+@\w+\.\w{2,3}\z/, message: "invalid email" }
+  validates :username, uniqueness: true
+end
+
+def stripspaces
+  self.email = email.lstrip.rstrip unless email.nil?
 end
