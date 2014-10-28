545a2128f402ed120e4f8317fb4588acceedeaf0
Sebastien <slemoan@gmail.com>
2014-10-28 11:40:01 +0100

    resultat

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
@@ -1,1 +1,20 @@
 # TODO: Write a seed
+require "json"
+require "rest-client"
+require "faker"
+
+# Create 5 fake users
+5.times do
+  user = User.new(username: Faker::Internet.user_name, email: Faker::Internet.email)
+  user.save
+end
+
+
+# generate 5 to 10 fake articles per user
+5.times do |i|
+  (5..10).to_a.sample.times do
+    post = Post.new(name: Faker::Lorem.sentence, url: Faker::Internet.url)
+    User.find(i + 1).posts << post
+    post.save
+  end
+end
